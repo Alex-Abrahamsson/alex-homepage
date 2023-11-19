0860e5dd4034b2c919f96e4006cb7d2895ca418b
Alex <Alex.abrahamsson@gmail.com>
2023-11-19 22:31:26 +0100

    Snaske

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,28 @@ import App from './App';
 
 import './index.scss';
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDzXEiZd4tkBg1flXOZP-rUoVK3HwT-13U",
+  authDomain: "alex-de3b8.firebaseapp.com",
+  projectId: "alex-de3b8",
+  storageBucket: "alex-de3b8.appspot.com",
+  messagingSenderId: "575344642315",
+  appId: "1:575344642315:web:589c48e1a35f354dbdfa0e",
+  measurementId: "G-KP6PF6EX7T"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
